675d10c9bb21452b0e84b029a87c1d0a0fb0d60b
Sam Boocker <sboocker@gmail.com>
2018-09-04 15:58:34 -0500

    changed modulous to modulus
    Changed all modulous in code to make consistent (and the correct spelling). (to modulus).

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -37,11 +37,11 @@ int isKaprekar(int n) {
   //for each possible "split" of the square...
   for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
-    first = square / modulous;
-    second = square % modulous;
+    first = square / modulus;
+    second = square % modulus;
 
     //test if the split makes a Kaprekar number
     if(second > 0 &&
