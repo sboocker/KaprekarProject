556b2d82ae20faec64303f775b0bccf65d23640b
Sam Boocker <sboocker@gmail.com>
2018-09-04 15:55:00 -0500

    Changed 'Modulus' to "Modulous"

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -5,21 +5,21 @@
 /**
  * <p>This function tests whether the given input <code>n</code>
  * is a Kaprekar Number or not.  A Kaprekar Number is a non-negative
- * integer n whose square can be split into two positive integers 
- * whose sum is equal to the original number.  For the purposes of 
+ * integer n whose square can be split into two positive integers
+ * whose sum is equal to the original number.  For the purposes of
  * this method, we restrict our consideration to base-10 representations.</p>
- * 
- * For example, 45 is a Kaprekar number, because 
- *   45<sup>2</sup> = 2025 
- * and 
- *   20 + 25 = 45. 
- * 
- * <p>See Wikipedia's  
+ *
+ * For example, 45 is a Kaprekar number, because
+ *   45<sup>2</sup> = 2025
+ * and
+ *   20 + 25 = 45.
+ *
+ * <p>See Wikipedia's
  * <a href="https://en.wikipedia.org/wiki/Kaprekar_number">Kaprekar Number</a>
  * article for more information.</p>
- * 
+ *
  * @param n
- * @return <code>true</code> if <code>n</code> is a Kaprekar Number, 
+ * @return <code>true</code> if <code>n</code> is a Kaprekar Number,
  * <code>false</code> otherwise.
  */
 int isKaprekar(int n) {
@@ -40,8 +40,8 @@ int isKaprekar(int n) {
     modulous *= 10;
 
     //split the square into two parts
-    first = square / modulus;
-    second = square % modulus;
+    first = square / modulous;
+    second = square % modulous;
 
     //test if the split makes a Kaprekar number
     if(second > 0 &&
@@ -50,5 +50,5 @@ int isKaprekar(int n) {
     }
   }
   return 0;
-  
+
 }
